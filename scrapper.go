9ab9d5ed5e6a22294cd5fa1091729c34aa172efc
DrEverr <ms1997sokol@gmail.com>
2022-12-24 21:52:21 +0100

    Extracting product data

--- scrapper.go
+++ scrapper.go
@@ -14,6 +14,7 @@ import (
 
 var queuePages []string
 var queueProducts []string
+var extractedProducts []Product
 var config Config
 
 // read file config.json and change it to struct
@@ -80,6 +81,8 @@ func scrape(url string) {
 		url = config.Url + url
 	}
 
+	log.Println("Scraping: " + url)
+
 	// get html
 	html, err := getHtml(url)
 	if err != nil {
@@ -111,16 +114,79 @@ func scrape(url string) {
 	}
 }
 
+// extract products
+func fetchProducts() {
+	for _, product := range queueProducts {
+		log.Println("Extracting: " + product)
+
+		// get html
+		html, err := getHtml(product)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		product := extractProductData(html)
+		if product.Name != "" && product.Price != "" {
+			extractedProducts = append(extractedProducts, product)
+		}
+	}
+}
+
+// extract product data
+func extractProductData(html string) Product {
+	var product Product
+
+	productHtml, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		log.Println(err)
+		return product
+	}
+
+	// get product name
+	if config.Product.Name != "" {
+		product.Name = find(productHtml, config.Product.Name)
+	} else {
+		log.Println("Product name not found in config.json")
+	}
+
+	// get product price
+	if config.Product.Price != "" {
+		product.Price = find(productHtml, config.Product.Price)
+	} else {
+		log.Println("Product price not found in config.json")
+	}
+
+	// get product sku
+	product.Sku = find(productHtml, config.Product.Sku)
+
+	return product
+}
+
+func find(doc *goquery.Document, selector string) string {
+	var text string
+	if strings.Contains(selector, "/") {
+		attribute := strings.Split(selector, "/")[1]
+		selector = strings.Split(selector, "/")[0]
+		doc.Find(selector).Each(func(_ int, s *goquery.Selection) {
+			text, _ = s.Attr(attribute)
+		})
+	} else {
+		doc.Find(selector).Each(func(_ int, s *goquery.Selection) {
+			text = s.Text()
+		})
+	}
+	return text
+}
+
 // main function
 func main() {
 	config = readConfig()
 	scrape(config.Url)
 
-	for _, url := range queuePages {
-		scrape(url)
+	for i := 0; i < len(queuePages); i++ {
+		scrape(queuePages[i])
 	}
 
-	// log config details
-	log.Println(queuePages)
-	log.Println(queueProducts)
+	fetchProducts()
 }
